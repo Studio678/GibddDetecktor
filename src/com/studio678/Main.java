510211d3444cd4dc758a28e158fb4c7cf5869b70
studio678 <murzinaa1987@gmail.com>
2019-05-21 19:13:08 +0500

    project works rev.a1 tag pa1

---
+++ src/com/studio678/Main.java
@@ -0,0 +1,53 @@
+/***
+ *
+ */
+
+
+
+package com.studio678;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+import static com.studio678.GenerationCarNumbers.generationCarNumbers;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // write your code here
+
+        ArrayList<String> carNumbers = generationCarNumbers();
+        long m;
+
+        String str = "";
+        while(!str.equals("exit") ) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Please type car number:");
+            str = reader.readLine().trim();
+            m = System.currentTimeMillis();
+            if(carNumbers.contains(str)){
+                System.out.println("found number in base true ");
+                System.out.print((double) (System.currentTimeMillis() - m));
+                System.out.println(" ms");
+            } else if(str.equals("exit")) {
+                break;
+            }else{
+                System.out.println("found number in base false ");
+                System.out.print((double) (System.currentTimeMillis() - m));
+                System.out.println(" ms");
+            }
+        }
+
+
+    }
+
+
+
+
+
+}
